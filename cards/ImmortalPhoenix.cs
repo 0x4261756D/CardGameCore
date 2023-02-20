6019c46ccc97125d6f52e12a91744b4e154453c4
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-02-21 00:52:34 +0100

    More more more more more cards

---
+++ cards/ImmortalPhoenix.cs
@@ -0,0 +1,18 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+
+class ImmortalPhoenix : Creature
+{
+	public ImmortalPhoenix() : base(
+		Name: "Immortal Phoenix",
+		CardClass: PlayerClass.Pyromancer,
+		OriginalCost: 3,
+		Text: "{Death}: Return this to your hand. For the rest of the game all \"Immortal Phoenix\" you control gain +1/+1. ",
+		OriginalPower: 2,
+		OriginalLife: 2
+		)
+	{ }
+	// TODO: implement functionality
+
+}
