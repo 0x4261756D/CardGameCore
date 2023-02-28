d5d5fae7abd45b7e283ec0dc49f7528b1c64cdca
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-02-28 02:49:56 +0100

    Partially implement Unholy Summon

--- cards/UnholySummoning.cs
+++ cards/UnholySummoning.cs
@@ -11,8 +11,22 @@ class UnholySummoning : Spell
 		Text: "{Cast}: Discard 1. Create 1 X/X Horror token where X is the discarded Card's Momentum cost.\n{Discard}: Create a 3/2 Horror Token."
 		)
 	{ }
+	// TODO: implement discard trigger
+
 	public override void Init()
 	{
+		RegisterCastTrigger(effect: CastEffect, condition: Condition, referrer: this);
 	}
 
+	private bool Condition()
+	{
+		return GetHand(Controller).Length > 0 && GetField(Controller).Any(x => x == null);
+	}
+
+	private void CastEffect()
+	{
+		Card target = SelectCards(cards: GetHand(Controller), amount: 1, player: Controller, description: "Select card to discard")[0];
+		Discard(target);
+		CreateToken(player: Controller, power: target.Cost, life: target.Cost, name: "Horror");
+	}
 }
