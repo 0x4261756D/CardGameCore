091e1a1f6f998d927e183dc3d2cb0dfd1bdde454
Dotlof <Dotloflp@gmail.com>
2023-03-09 20:01:57 +0100

    Added Scripts for two cards

--- cards/EssenceDrainer.cs
+++ cards/EssenceDrainer.cs
@@ -13,10 +13,23 @@ class EssenceDrainer : Creature
 		OriginalLife: 1
 		)
 	{ }
-	// TODO: implement functionality
 
 	public override void Init()
 	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect, condition: CastCondition), referrer: this);
+	}
+
+	public void CastEffect(){
+		RegisterTemporaryLingeringEffect(info: new LingeringEffectInfo(effect: BuffEffect, referrer: this));
+	}
+
+	public void BuffEffect(Card target){
+		target.Life += GetDiscardCountThisTurn(player: Controller);
+		target.Power += GetDiscardCountThisTurn(player: Controller);
+	}
+
+	public bool CastCondition(){
+		return true;
 	}
 
 }
