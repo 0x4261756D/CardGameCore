597fbb5833b4ba4cd1eb441b176ee09110127ba9
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-03-14 17:48:28 +0100

    Change default return value for GetIgniteDamage to -1
    This increases the probability to catch stupid errors where this effect is not overridden

--- Card.cs
+++ Card.cs
@@ -107,7 +107,7 @@ public abstract class Card
 	public DrawDelegate Draw = (_, _) => { };
 	public DestroyDelegate Destroy = (_) => { };
 	public AskYesNoDelegate AskYesNo = (_, _) => false;
-	public GetIgniteDamageDelegate GetIgniteDamage = (_) => 0;
+	public GetIgniteDamageDelegate GetIgniteDamage = (_) => -1;
 	public ChangeIgniteDamageDelegate ChangeIgniteDamage = (_, _) => { };
 
 	public void ClearModifications()
