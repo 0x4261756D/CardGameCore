8b199fb1680b992a38ae7d53f0093d1a9d8ca9de
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-03-14 15:31:35 +0100

    Add ContainsValid and FilterValid utility functions

--- CardUtils.cs
+++ CardUtils.cs
@@ -11,4 +11,14 @@ public class CardUtils
 	{
 		return cards.Any(x => x != null);
 	}
+
+	public delegate bool IsValid(Card card);
+	public static bool ContainsValid(Card[] cards, IsValid isValid)
+	{
+		return cards.Any(x => isValid(x));
+	}
+	public static Card[] FilterValid(Card[] cards, IsValid isValid)
+	{
+		return cards.Where(x => isValid(x)).ToArray();
+	}
 }
