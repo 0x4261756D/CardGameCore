b9088cc1814d811e36534a330253d7a4aeb7ab46
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-03-15 12:31:12 +0100

    Implement DarkBolt

--- cards/DarkBolt.cs
+++ cards/DarkBolt.cs
@@ -1,6 +1,7 @@
 // Scripted by 0x4261756D
 using CardGameCore;
 using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
 
 class DarkBolt : Spell
 {
@@ -11,10 +12,33 @@ class DarkBolt : Spell
 		Text: "{Cast}: Deal 3 damage any target.\n{Discard}: Cast this."
 		)
 	{ }
-	// TODO: implement functionality
 
 	public override void Init()
 	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect), referrer: this);
+		RegisterDiscardTrigger(trigger: new DiscardTrigger(effect: DiscardEffect), referrer: this);
 	}
 
+	private void DiscardEffect()
+	{
+		Cast(player: Controller, card: this);
+	}
+
+	private void CastEffect()
+	{
+		if(HasUsed(GetBothWholeFields()) && AskYesNo(player: Controller, question: "Target a creature?"))
+		{
+			Card target = SelectCards(player: Controller, cards: GetForBoth(GetFieldUsed), amount: 1, description: "Select target")[0];
+			RegisterTemporaryLingeringEffect(info: new LingeringEffectInfo(effect: DamageEffect, referrer: target));
+		}
+		else
+		{
+			PlayerChangeLife(player: AskYesNo(player: Controller, question: "Damage opponent?") ? 1 - Controller : Controller, amount: 3);
+		}	
+	}
+
+	private void DamageEffect(Card target)
+	{
+		target.Life -= 3;
+	}
 }
