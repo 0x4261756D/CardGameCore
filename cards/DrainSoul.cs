f82315b52c7c17348d14f16de1a9fc86ee866d0a
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-03-16 13:45:27 +0100

    Implement DrainSoul

--- cards/DrainSoul.cs
+++ cards/DrainSoul.cs
@@ -1,6 +1,7 @@
 // Scripted by 0x4261756D
 using CardGameCore;
 using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
 
 class DrainSoul : Spell
 {
@@ -8,13 +9,36 @@ class DrainSoul : Spell
 		Name: "Drain Soul",
 		CardClass: PlayerClass.Cultist,
 		OriginalCost: 1,
-		Text: "{Cast}: Sacrifice 1 Creature. Gain life equal to its attack.\n{Discard}: Gain 3 life.\n{Revelation}: Gain 1 life."
+		Text: "{Cast}: Destroy 1 creature. Gain life equal to its power.\n{Discard}: Gain 3 life.\n{Revelation}: Gain 1 life."
 		)
 	{ }
-	// TODO: implement functionality
 
 	public override void Init()
 	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect, condition: CastCondition), referrer: this);
+		RegisterDiscardTrigger(trigger: new DiscardTrigger(effect: DiscardEffect), referrer: this);
+		RegisterRevelationTrigger(trigger: new RevelationTrigger(effect: RevelationEffect), referrer: this);
 	}
 
+	private void RevelationEffect()
+	{
+		PlayerChangeLife(player: Controller, amount: 1);
+	}
+
+	private void DiscardEffect()
+	{
+		PlayerChangeLife(player: Controller, amount: 3);
+	}
+
+	private void CastEffect()
+	{
+		Card target = SelectCards(player: Controller, cards: GetForBoth(GetFieldUsed), amount: 1, description: "Select creature to drain")[0];
+		Destroy(target);
+		PlayerChangeLife(player: Controller, amount: target.Power);
+	}
+
+	private bool CastCondition()
+	{
+		return HasUsed(GetBothWholeFields());
+	}
 }
