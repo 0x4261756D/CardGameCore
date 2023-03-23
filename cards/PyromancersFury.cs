362986348386e2a162d6abf33fd917389dcc8bf1
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-03-23 22:34:30 +0100

    Add PyromancersFury

--- cards/PyromancersFury.cs
+++ cards/PyromancersFury.cs
@@ -4,16 +4,39 @@ using static CardGameUtils.GameConstants;
 
 class PyromancersFury : Spell
 {
+	private bool discardTriggerActive = false;
 	public PyromancersFury() : base(
 		Name: "Pyromancer's Fury",
 		CardClass: PlayerClass.Pyromancer,
 		OriginalCost: 1,
-		Text: "Your \"Ignite\" deals +1 damage for the rest of the game. {A creature dies this turn}: Your ability is refreshed."
+		Text: "{Cast}: Your \"Ignite\" deals +1 damage for the rest of the game. {A creature dies this turn}: Your ability is refreshed."
 		)
 	{ }
-	// TODO: implement functionality
 
 	public override void Init()
 	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect), referrer: this);
+		RegisterLingeringEffect(info: new LingeringEffectInfo(effect: LingeringEffect, referrer: this, influenceLocation: Location.ALL));
+	}
+
+	public void LingeringEffect(Card target)
+	{
+		if(!discardTriggerActive)
+		{
+			return;
+		}
+		RefreshAbility(Controller);
+	}
+
+	public void CastEffect()
+	{
+		ChangeIgniteDamage(player: Controller, amount: 1);
+		discardTriggerActive = true;
+		RegisterTemporaryLingeringEffect(info: new LingeringEffectInfo(effect: ResetEffect, referrer: this,  influenceLocation: Location.ALL));
+	}
+
+	public void ResetEffect(Card _)
+	{
+		discardTriggerActive = false;
 	}
 }
