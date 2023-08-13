9fae25b29042a8227e707dfc8abcc66a5cdc9bd1
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-13 18:55:10 +0200

    Add Masterpiece

---
+++ cards/Masterpiece.cs
@@ -0,0 +1,36 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class Masterpiece : Spell
+{
+	public Masterpiece() : base(
+		Name: "Masterpiece",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 5,
+		Text: "{Cast}: Target a creature in your Grave, create any number X/X token copies of it (min. 1), where X is the sum of the target's Power and Life."
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect, condition: CastCondition), referrer: this);
+	}
+
+	private void CastEffect()
+	{
+		Card target = SelectSingleCard(Controller, GetGrave(Controller), "Select masterpiece");
+		while(HasEmpty(GetField(Controller)) && AskYesNo(player: Controller, question: "Create another?"))
+		{
+			Card token = CreateTokenCopy(player: Controller, target);
+			CreatureChangeLife(target: token, amount: target.Power, source: this);
+			RegisterLingeringEffect(new LingeringEffectInfo(effect: (t) => t.Power += target.Life, referrer: token));
+		}
+	}
+
+	private bool CastCondition()
+	{
+		return GetGrave(Controller).Length > 0 && HasEmpty(GetField((Controller)));
+	}
+}
