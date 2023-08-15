76a0583f835728bb195e74c7b9dbcbf8cba8b6c0
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-15 06:53:50 +0200

    Add MechanicalFamiliar

---
+++ cards/MechanicalFamiliar.cs
@@ -0,0 +1,42 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class MechanicalFamiliar : Creature
+{
+	public MechanicalFamiliar() : base(
+		Name: "Mechanical Familiar",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 1,
+		Text: "{You create a token with [Brittle]}: You may create another one.\n{Revelation}: Add this to your hand.",
+		OriginalPower: 1,
+		OriginalLife: 2
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterGenericCastTrigger(trigger: new GenericCastTrigger(effect: CreateEffect, condition: CreateCondition), referrer: this);
+		RegisterRevelationTrigger(trigger: new RevelationTrigger(effect: RevelationEffect), referrer: this);
+	}
+
+	private void RevelationEffect()
+	{
+		MoveToHand(player: Controller, card: this);
+	}
+
+	private bool CreateCondition(Card target)
+	{
+		return target.Controller == Controller && target.Keywords.ContainsKey(Keyword.Token) && target.Keywords.ContainsKey(Keyword.Brittle) &&
+			HasEmpty(GetField(Controller));
+	}
+
+	private void CreateEffect(Card target)
+	{
+		if(AskYesNo(player: Controller, question: "Create another one?"))
+		{
+			CreateTokenCopy(player: Controller, card: target);
+		}
+	}
+}
