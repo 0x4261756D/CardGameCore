0e52ce555cae6dfc3fe7c46b5303c2d69dfe1f45
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-15 06:59:59 +0200

    Add Replicate

---
+++ cards/Replicate.cs
@@ -0,0 +1,31 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class Replicate : Spell
+{
+	public Replicate() : base(
+		Name: "Replicate",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 2,
+		Text: "{Cast}: Create token copy of your target creature."
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect, condition: CastCondition), referrer: this);
+	}
+
+	private void CastEffect()
+	{
+		Card target = SelectSingleCard(player: Controller, GetFieldUsed(player: 1 - Controller), description: "Select card to replicate");
+		CreateTokenCopy(player: Controller, card: target);
+	}
+
+	private bool CastCondition()
+	{
+		return HasEmpty(GetField(Controller)) && HasUsed(GetField(Controller));
+	}
+}
