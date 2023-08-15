11d1c143fad006a4d8df600c5ba7372e275ebe8c
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-15 07:23:32 +0200

    Add Equalize

---
+++ cards/Equalize.cs
@@ -0,0 +1,41 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class Equalize : Spell
+{
+	public Equalize() : base(
+		Name: "Equalize",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 4,
+		Text: "{Cast}: The player controlling the most creatures returns creatures to their hand until the players control the same amount of creatures.\n{Revelation}: Draw X cards where X is the difference between your and your opponent's amount of creatures."
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect), referrer: this);
+		RegisterRevelationTrigger(trigger: new RevelationTrigger(effect: RevelationEffect), referrer: this);
+	}
+
+	private void RevelationEffect()
+	{
+		Draw(player: Controller, amount: Math.Abs(GetFieldUsed(0).Length - GetFieldUsed(1).Length));
+	}
+
+	private void CastEffect()
+	{
+		int difference = GetFieldUsed(1).Length - GetFieldUsed(0).Length;
+		if(difference == 0)
+		{
+			return;
+		}
+		int amount = Math.Abs(difference);
+		int player = ((difference / amount) + 1) / 2;
+		for(int i = 0; i < amount; i++)
+		{
+			MoveToHand(player: player, card: SelectSingleCard(player: player, cards: GetFieldUsed(player), description: "Select card to return to hand"));
+		}
+	}
+}
