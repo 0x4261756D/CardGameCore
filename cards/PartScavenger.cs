d7db43c72d636fe7f39dddf50ecc0c4315e2f3c5
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-15 06:39:30 +0200

    Add PartScavenger

---
+++ cards/PartScavenger.cs
@@ -0,0 +1,38 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class PartScavenger : Creature
+{
+	public PartScavenger() : base(
+		Name: "Part Scavenger",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 2,
+		Text: "{Your creature dies}: Gain +2/+2.",
+		OriginalPower: 2,
+		OriginalLife: 2
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterGenericDeathTrigger(trigger: new GenericDeathTrigger(effect: DeathEffect, condition: DeathCondition), referrer: this);
+	}
+
+	private bool DeathCondition(Card target)
+	{
+		return target.Controller == Controller;
+	}
+
+	private void DeathEffect(Card target)
+	{
+		RegisterTemporaryLingeringEffect(info: new LingeringEffectInfo(effect: BoostEffect, referrer: this));
+	}
+
+	private void BoostEffect(Card target)
+	{
+		target.Life += 2;
+		target.Power += 2;
+	}
+}
