8411258d96fc531599c7b53fa98ffed54a36865d
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-15 05:54:07 +0200

    Add Recalibrate

---
+++ cards/Recalibrate.cs
@@ -0,0 +1,31 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class Recalibrate : Spell
+{
+	public Recalibrate() : base(
+		Name: "Recalibrate",
+		CardClass: PlayerClass.Artificer,
+		OriginalCost: 2,
+		Text: "{Cast}: Take control of target creature. It gains [Brittle]."
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: CastEffect, condition: CastCondition), referrer: this);
+	}
+
+	private void CastEffect()
+	{
+		Card target = SelectSingleCard(player: Controller, GetFieldUsed(player: 1 - Controller), description: "Select card to recalibrate");
+		MoveToField(choosingPlayer: Controller, targetPlayer: Controller, card: target);
+	}
+
+	private bool CastCondition()
+	{
+		return HasEmpty(GetField(Controller)) && HasUsed(GetField(1 - Controller)); 
+	}
+}
