c461e35b22460e52d624dde3f56144cfa8d74bb9
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-21 10:25:07 +0200

    Add Cartist

---
+++ cards/Cartist.cs
@@ -0,0 +1,21 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+
+class Cartist : Creature
+{
+	public Cartist() : base(
+		Name: "Cartist",
+		CardClass: PlayerClass.All,
+		OriginalCost: 2,
+		Text: "{Cast}: Draw 1.",
+		OriginalPower: 2,
+		OriginalLife: 1
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterCastTrigger(trigger: new CastTrigger(effect: () => Draw(player: Controller, amount: 1)), referrer: this);
+	}
+}
