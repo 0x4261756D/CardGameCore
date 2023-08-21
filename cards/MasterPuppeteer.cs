41f62a8cd5fd01fe0ce0fa9e80d7b84448d3d8e5
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-08-21 10:25:17 +0200

    Add Master Puppeteer

---
+++ cards/MasterPuppeteer.cs
@@ -0,0 +1,35 @@
+// Scripted by 0x4261756D
+using CardGameCore;
+using static CardGameUtils.GameConstants;
+using static CardGameCore.CardUtils;
+
+class MasterPuppeteer : Creature
+{
+	public MasterPuppeteer() : base(
+		Name: "Master Puppeteer",
+		CardClass: PlayerClass.All,
+		OriginalCost: 3,
+		Text: "{Activate}: Move target creature.",
+		OriginalPower: 3,
+		OriginalLife: 2
+		)
+	{ }
+
+	public override void Init()
+	{
+		RegisterActivatedEffect(info: new ActivatedEffectInfo(name: "Activate", effect: MoveEffect, condition: MoveCondition, referrer: this));
+	}
+
+	private void MoveEffect()
+	{
+		Card target = SelectSingleCard(player: Controller, cards: FilterValid(GetForBoth(GetFieldUsed), FilterMovable), "Select target to move");
+		MoveToField(choosingPlayer: Controller, targetPlayer: target.Controller, card: (Creature)target, source: this);
+	}
+
+	private bool FilterMovable(Card card) => !card.Keywords.ContainsKey(Keyword.Immovable);
+
+	private bool MoveCondition()
+	{
+		return ContainsValid(cards: GetForBoth(GetFieldUsed), isValid: FilterMovable);
+	}
+}
