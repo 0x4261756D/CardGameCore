4650fa5d6553921cfd01158b1ec039c20dcfc794
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-12-12 21:44:34 +0100

    Improve Masterpiece

--- cards/Masterpiece.cs
+++ cards/Masterpiece.cs
@@ -17,16 +17,23 @@ class Masterpiece : Spell
 	{
 		RegisterCastTrigger(trigger: new Trigger(effect: CastEffect, condition: CastCondition), referrer: this);
 	}
-
+	private void CreateMasterpiece(Creature target)
+	{
+		Creature token = CreateTokenCopy(player: Controller, target);
+		CreatureChangeLife(target: token, amount: target.Power, source: this);
+		CreatureChangePower(target: token, amount: target.Life, source: this);
+		MoveToField(targetPlayer: Controller, choosingPlayer: Controller, card: token, source: this);
+	}
 	private void CastEffect()
 	{
 		Creature target = (Creature)SelectSingleCard(Controller, FilterValid(cards: GetGrave(Controller), isValid: (card) => card.CardType == CardType.Creature), "Select masterpiece");
+		if(HasEmpty(GetField(Controller)) && AskYesNo(player: Controller, question: "Create a masterpiece?"))
+		{
+			CreateMasterpiece(target);
+		}
 		while(HasEmpty(GetField(Controller)) && AskYesNo(player: Controller, question: "Create another?"))
 		{
-			Creature token = CreateTokenCopy(player: Controller, target);
-			CreatureChangeLife(target: token, amount: target.Power, source: this);
-			CreatureChangePower(target: token, amount: target.Life, source: this);
-			MoveToField(targetPlayer: Controller, choosingPlayer: Controller, card: token, source: this);
+			CreateMasterpiece(target);
 		}
 	}
 
